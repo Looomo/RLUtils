83e194d5a8162cf62d4e8269181e9cd2a3ff29ef
Looomo <shanyixiang@gmail.com>
2025-01-04 16:19:54 +0800

    支持自动fall back到同系列branch

--- RLUtils/branch_control.py
+++ RLUtils/branch_control.py
@@ -1,5 +1,6 @@
 import json
 import os
+import logging
 class FallbackController(object):
     def __init__(self, load_path = None) -> None:
         self.fall_back_configs = {}
@@ -16,8 +17,23 @@ class FallbackController(object):
             fall_back_config = file
         self.fall_back_configs[fall_back_config['name']] = fall_back_config
     
-    def get_fallback(self, config_name, item_name, branch):
-        
-        fallback_level = self.fall_back_configs[config_name][item_name]["fallbacks"][branch]
-        plan = self.fall_back_configs[config_name][item_name][fallback_level]
-        return plan
+    def get_fallback(self, config_name, item_name, branch, enable_default = False, enable_auto_fallback = False):
+        assert len(branch) > 0
+        if enable_default:
+            fallback_level = self.fall_back_configs[config_name][item_name]["fallbacks"].get(branch, "default")
+            plan = self.fall_back_configs[config_name][item_name][fallback_level]
+            return plan
+        elif enable_auto_fallback:
+            if branch in self.fall_back_configs[config_name][item_name]["fallbacks"].keys():
+                fallback_level = self.fall_back_configs[config_name][item_name]["fallbacks"].get(branch)
+                plan = self.fall_back_configs[config_name][item_name][fallback_level]
+                return plan
+            else:
+                fall_back_branch = branch[:-1]
+                logging.warning(f"\033[33m RLUtils.FallbackController: Item \033[34m{config_name}.{item_name}\033[33m fallbacking from \033[34m{branch}\033[33m to \033[34m{fall_back_branch}\033[33m.\033[0m")
+                return self.get_fallback(  config_name, item_name, fall_back_branch, enable_default, enable_auto_fallback)
+            
+        else:
+            fallback_level = self.fall_back_configs[config_name][item_name]["fallbacks"].get(branch)
+            plan = self.fall_back_configs[config_name][item_name][fallback_level]
+            return plan
